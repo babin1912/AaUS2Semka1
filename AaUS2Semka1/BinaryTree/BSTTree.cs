e9aef027c58b23d279c5e5cc1293692e0be81b8c
Matúš Babinský <matusbabinsky@yahoo.com>
2019-10-11 13:20:24 +0200

    blyat

--- AaUS2Semka1/BinaryTree/BSTTree.cs
+++ AaUS2Semka1/BinaryTree/BSTTree.cs
@@ -46,20 +46,48 @@ namespace AaUS2Semka1.BinaryTree
 
         public void ToStringAF()
         {
-
+            //LevelOrder();
+            var input = LevelOrder();
             bool ch = true;
-            foreach (var level in LevelOrder())
+            int i = input.Count;
+            bool first = true;
+            
+            foreach (var level in input)
             {
-                
+                //Console.Write(Math.Pow((double)2.0, (double)i - 2)+1);
                 foreach (var node in level)
                 {
+                    if (!ch && first)
+                    {
+                        Console.Write("|".PadLeft(((int)Math.Pow((double)2.0, (double)i - 1) * 6)/2 - 2, ' '));
+                        
+                        first = false;
+                    }
+                    else if(!ch && !first)
+                    {
+                        if (i==1)
+                        {
+                            //Console.Write(i);
+                            Console.Write("".PadLeft(((int)Math.Pow((double)2.0, (double)i - 2) * 6), ' '));
+
+                        }
+                        else
+                        {
+                            
+                            Console.Write("|".PadLeft(((((int)Math.Pow((double)2.0, (double)i - 2)) * 6)), ' '));
+                            //Console.Write((int)Math.Pow((double)2.0, (double)i - 2) * 6);
+
+                        }
+
+                    }
+
                     if (node ==null)
                     {
                         Console.Write("__");
                     }
                     else 
                     {
-                        Console.Write(string.Concat(node.ToString(), " "));
+                        Console.Write(node.ToString().PadLeft(2, '0'));
                     }
 
                     if (ch)
@@ -69,17 +97,22 @@ namespace AaUS2Semka1.BinaryTree
                     }
                     else
                     {
-                        Console.Write(" ");
+                         Console.Write(" ");
                         ch = true;
                     }
 
                     
-                    
-                    
+
+
+
+
                 }
+                first = true;
+                i--;
                 Console.WriteLine();
             }
             
+
             //return "";
         }
 
@@ -87,10 +120,11 @@ namespace AaUS2Semka1.BinaryTree
         {
             List<List<object>> product = new List<List<object>>();
             List<object> level = new List<object>();
-            level.Add(root);
+            //level.Add(root);
             product.Add(new List<object>());
             product[0].Add(root);
             bool work;
+            //Console.WriteLine(root.ToString().PadLeft(2, '0'));
             do
             {
                 work = false;
@@ -102,6 +136,7 @@ namespace AaUS2Semka1.BinaryTree
                         if (((BSTNode)node).HasLeftChild())
                         {
                             level.Add(((BSTNode)node).LeftChild );
+                            //Console.Write(((BSTNode)node).LeftChild.ToString().PadLeft(2, '0'));
                             work = true;
                         }
                         else
@@ -109,31 +144,41 @@ namespace AaUS2Semka1.BinaryTree
                             level.Add(null);
                         }
 
+                        //Console.Write(" ");
                         if (((BSTNode)node).HasRightChild())
                         {
                             level.Add(((BSTNode)node).RightChild);
+                            //Console.Write(((BSTNode)node).RightChild.ToString().PadLeft(2, '0'));
                             work = true;
                         }
                         else
                         {
                             level.Add(null);
+
                         }
+                        //Console.Write("|");
                     
                     }
                     else
                     {
+                        //Console.Write("__ __");
                         level.Add(null);
                         level.Add(null);
+
                     }
 
                 }
 
+                //Console.WriteLine();
+
                 if (work)
                 {
+                    //product = new List<List<object>>();
                     product.Add(level);
+                    level = new List<object>();
                 }
                 
-                level = new List<object>();
+                
 
 
             } while (work);
